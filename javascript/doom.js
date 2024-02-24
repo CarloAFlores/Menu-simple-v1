fb95c678d5ccb44c60dbce557c7e3eb381d959e5
unknown <carlos2000sistemasc@gmail.com>
2024-02-23 21:41:39 -0800

    Se agrego el local storage

--- javascript/doom.js
+++ javascript/doom.js
@@ -1,4 +1,3 @@
-
 const apiMenu = async(idBoton)=>{
 
     const resp =await fetch("MENU.json");
@@ -10,6 +9,7 @@ const apiMenu = async(idBoton)=>{
 
 
 document.addEventListener('DOMContentLoaded',()=>{
+    
     let botones = document.querySelectorAll('.Menu__opcion');
     
     botones.forEach(boton => {
@@ -30,7 +30,6 @@ document.addEventListener('DOMContentLoaded',()=>{
         }
     });
 
-
 })
 
 function abrirModal(modal){
@@ -47,11 +46,40 @@ function BuscadorAPI(idBoton){
     console.log(idBoton)
     apiMenu(idBoton);
 }
+let arrayCompra = [];
 
 function MaquetarCuerpo(SubMenu) {
     let maqueta = '';
 
     SubMenu.forEach(element => {
-        console.log(element);
+        const {name, description, price} = element;
+        maqueta += `<div class="card">
+            <img src="https://via.placeholder.com/600/92c952" alt="" class="card__img">
+            <span class="card__nombre">${name}</span>
+            <p class="card__descripcion">${description}</p>
+            <span class="card__precio precio">$${price}</span>
+            <button class="card__compra btn" data-price=${price}>AGREGAR</button>
+        </div>`;
     });
+
+    document.getElementById('modal__info').innerHTML = maqueta;
+
+    let botonesAgregarCarro = document.querySelectorAll('.btn');
+
+    botonesAgregarCarro.forEach(boton => {
+        boton.addEventListener('click', () => {
+            const price = boton.getAttribute('data-price');
+            arrayCompra.push(price)
+            console.log(price);
+            Suma(arrayCompra);
+        });
+    });
+}
+
+function Suma(arrayCompra) {
+    console.log(arrayCompra);
+    // Convertir cada elemento del array a número usando parseFloat
+    let total = arrayCompra.reduce((a, b) => parseFloat(a) + parseFloat(b), 0);
+    console.log(total);
+    localStorage.setItem("titulo",total);
 }
